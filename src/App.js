16818b95361d6e567e6cf00abf7676c1d1a66dd2
Jin <qkrrbekrrb.coder.qkrrbekrrbzz@gmail.com>
2021-12-20 15:29:53 +0900

    withStyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 
-import './App.css';
-
+import React, { Component } from 'react';
 import Customer from './components/Customer';
+import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+import { withStyles } from '@mui/styles';
+const styles = theme => ({ 
+  root: {
+    width: '100%',
+    marginTop: 20,
+    overflowX: "auto"
+   },
+  table:{
+    minWidth: 1080
+  }
+});
 
 const customers =[
   {
@@ -30,30 +48,34 @@ const customers =[
 
 
 ]
-
-function App() {
+class App extends Component{
+render() {
+  const { classes } = this.props;
   return (
-    <div>
-  {
-    customers.map(c =>{
-      return(
-      <Customer
-      key={c.id}
-      id={c.id}
-      name={c.name}
-      birthday={c.birthday}
-      gender={c.gender}
-      job={c.job}
-      image={c.image}
+    <Paper className= {classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+         <TableRow>
+           <TableCell>번호</TableCell>
+           <TableCell>이미지</TableCell>
+           <TableCell>이름</TableCell>
+           <TableCell>생년월일</TableCell>
+           <TableCell>성별</TableCell>
+           <TableCell>직업</TableCell>
 
-      
-      />
-      );
-    })
-  }
-</div>
 
-  );
+         </TableRow>
+
+        </TableHead>
+        <TableBody>
+      {customers.map(c =>{return(<Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} image={c.image}/>); })}
+      </TableBody>
+      </Table>
+    
+    </Paper>
+
+   );
+ }
 }
 
-export default App;
+export default withStyles(styles)(App);
